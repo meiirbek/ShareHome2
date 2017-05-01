9464c1483b183dde5c96583dc7317a245a7322f3
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-04-30 22:28:18 -0500

    Improved profile design.

--- initialLoginVC.swift
+++ initialLoginVC.swift
@@ -12,25 +12,16 @@ import FBSDKCoreKit
 
 class initialLoginVC: UIViewController, FBSDKLoginButtonDelegate {
 
-    @IBOutlet weak var loginName: UITextField!
-    @IBOutlet weak var signUpButton: UIButton!
-    
-    @IBOutlet weak var passwordField: UITextField!
-    
-    @IBAction func signUpPressed(_ sender: UIButton) {
-//        UserDefaults.standard.set(loginName.text, forKey: "username")
-//        performSegue(withIdentifier: "toMainSegue", sender: self)
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        signUpButton.layer.cornerRadius = 5
-//        signUpButton.clipsToBounds = true
+        
         
         let loginButton = FBSDKLoginButton()
         loginButton.center = self.view.center
         self.view.addSubview(loginButton)
-        loginButton.delegate = self   
+        loginButton.delegate = self
+        
     }
     
    
