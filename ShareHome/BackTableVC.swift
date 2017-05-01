144d94648f5fa450f76aa0a18203fa3055bb8fc4
meiirbek <meiirbek@Meiirbeks-MacBook-Pro.local>
2017-05-01 04:43:23 -0500

    Implemented custom cells for back table view.

--- ShareHome/BackTableVC.swift
+++ ShareHome/BackTableVC.swift
@@ -22,18 +22,18 @@ class BackTableVC: UITableViewController {
         self.navigationController?.navigationBar.layer.shadowRadius = 2
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TableArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
-        
-        cell.textLabel?.text = TableArray[indexPath.row]
-
-        
-        return cell
-    }
-    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return TableArray.count
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
+//        
+//        cell.textLabel?.text = TableArray[indexPath.row]
+//
+//        
+//        return cell
+//    }
+//    
     
 }
