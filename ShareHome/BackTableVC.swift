655ccfec4e44ab33318f18705959c7e63fbd1811
meiirbek <meiirbek@meiirbeks-macbook-pro.local>
2017-06-25 21:03:28 -0500

    removed navigation controller!

--- ShareHome/BackTableVC.swift
+++ ShareHome/BackTableVC.swift
@@ -14,29 +14,24 @@ class BackTableVC: UITableViewController {
     var TableArray = [String] ()
     
     override func viewDidLoad() {
-        TableArray = ["Home","Profile","Make a Post", "Schedule","Grocery List","Purchases"]
-        
+        //TableArray = ["Home","Profile","Make a Post", "Schedule","Grocery List","Purchases"]
+        TableArray = ["topPart","Home", "Profile","Make a Post","Schedule", "Grocery List", "Purchases", "Logout"]
         self.navigationController?.navigationBar.layer.masksToBounds = false
         self.navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
         self.navigationController?.navigationBar.layer.shadowOpacity = 0.8
         self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0, height: 2.0)
         self.navigationController?.navigationBar.layer.shadowRadius = 2
         
-        
     }
     
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return TableArray.count
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
-//        
-//        cell.textLabel?.text = TableArray[indexPath.row]
-//
-//        
-//        return cell
-//    }
-//    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableArray[indexPath.row], for: indexPath) as UITableViewCell
+        return cell
+    }
+    
     
 }
